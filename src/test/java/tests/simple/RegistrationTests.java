8bee92930b5e287cc5ea659bcaa0ea0e396396ed
Alina <alicia1997@mail.ru>
2025-09-18 21:16:00 +0300

    add tag simple for reg test

--- src/test/java/tests/simple/RegistrationTests.java
+++ src/test/java/tests/simple/RegistrationTests.java
@@ -6,6 +6,7 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -45,6 +46,7 @@ public class RegistrationTests extends TestBase {
         Attach.addVideo();
     }
 
+    @Tag("simple")
     @Test
     void successfulRegistrationTest() {
         open("/automation-practice-form");
